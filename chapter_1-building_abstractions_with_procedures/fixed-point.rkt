5e59634244a6485f733bb8b50c22d9c72f93b986
HuyDNA <huydo862003@gmail.com>
2024-12-18 12:04:51 +0700

    fix: wrong closing parenthese placement

--- chapter_1-building_abstractions_with_procedures/fixed-point.rkt
+++ chapter_1-building_abstractions_with_procedures/fixed-point.rkt
@@ -1,10 +1,10 @@
 (define (fixed-point f first-guess)
   (define (close-enough? a b)
     (< (abs - a b)
-       0.0001)
+       0.0001))
   (define (try guess)
     (let ((next (f guess)))
       (if (close-enough? guess next)
         next
-        (try next)))))
+        (try next))))
   (try first-guess))
